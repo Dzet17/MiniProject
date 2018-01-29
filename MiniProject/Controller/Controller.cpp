c48dbf822376f88e95b74a0751d79e8a5cee7575
Dylan Zettler <dylanzettler@gmail.com>
2018-01-29 08:35:20 -0700

    Added pointer

--- MiniProject/Controller/Controller.cpp
+++ MiniProject/Controller/Controller.cpp
@@ -28,3 +28,16 @@ void Controller :: start()
 
 std::vector<std::string> data;
 
+int main ()
+{
+    int firstvalue, secondvalue;
+    int * mypointer;
+    
+    mypointer = &firstvalue;
+    *mypointer = 10;
+    mypointer = &secondvalue;
+    *mypointer = 20;
+    cout << "firstvalue is " << firstvalue << '\n';
+    cout << "secondvalue is " << secondvalue << '\n';
+    return 0;
+}
